c7cfc2dc964e666fb0a91afe399e2cea8f265701
Jorge Sard├│n <anibal317@gmail.com>
2022-05-30 23:57:21 -0300

    Creando el componente del cart

---
+++ e-commerce/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export class CartWidget extends Component {
+	render() {
+		return (
+			<img src="./src/imgs/shopping-cart.png"></img>
+		);
+	}
+}
+
+export default CartWidget;
