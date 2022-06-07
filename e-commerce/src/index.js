cb6143c6c17454f9216167a88748aeba82386fa5
Jorge Sard├│n <anibal317@gmail.com>
2022-06-07 18:25:04 -0300

    Uso de state para el count

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import NavBar from './components/NavBar/NavBar.jsx';
 
 import reportWebVitals from './reportWebVitals';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+// import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <NavBar />
-    <ItemListContainer />
+    {/* <ItemListContainer /> */}
     <App />
   </React.StrictMode>
 );
